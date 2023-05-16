e87cf453312086f7e82d0323e7fc65c15014fac3
negru-ioan <ioannegru808@gmail.com>
2023-05-16 11:18:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import Board from "./Board";
+import "./App.css";
+
+let App = function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.slice(0, currentMove).map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Mișcarea " + move;
+    } else {
+      description = "Resetează";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ul>{moves}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
